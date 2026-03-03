871ab64e0061274fad112f9ccc87d075a1b8a084
alpha <georgeofori2005@gmail.com>
2026-03-03 03:31:51 -0800

    about page underway

---
+++ scripts/about.js
@@ -0,0 +1,22 @@
+$(document).ready(function(event){
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(e => {
+		if (e.isIntersecting){
+			e.target.style.transform = "translateY(0)";
+			e.target.style.opacity = "1";
+			observer.unobserve(e.target)
+		}
+	});
+},
+{
+	threshold: 0.2
+}
+);
+
+observer.observe($(".s1-description")[0]);
+observer.observe($(".section-2")[0]);
+$(".bio").each((idx, ele) => {
+	observer.observe(ele);
+});
+observer.observe($(".section-2 img")[0]);
+});
